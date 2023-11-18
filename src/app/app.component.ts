0893fda339cd09da96a40d11f0aeb8828090aa0e
tavodevcode <tavodevcode@gmail.com>
2023-11-18 02:23:31 -0600

    📝 conf tailwind and eslintrc

--- src/app/app.component.ts
+++ src/app/app.component.ts
@@ -1,6 +1,7 @@
-import { Component } from '@angular/core';
+import { Component, OnInit } from '@angular/core';
 import { CommonModule } from '@angular/common';
 import { RouterOutlet } from '@angular/router';
+import { initFlowbite } from 'flowbite';
 
 @Component({
   selector: 'app-root',
@@ -9,6 +10,14 @@ import { RouterOutlet } from '@angular/router';
   templateUrl: './app.component.html',
   styleUrl: './app.component.scss'
 })
-export class AppComponent {
+export class AppComponent implements OnInit {
   title = 'crm-app';
+
+
+
+  ngOnInit(): void {
+    initFlowbite();
+
+    console.log("initFlowbite");
+  }
 }
