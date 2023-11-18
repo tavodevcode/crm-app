60b048e49f0d963f741e70370657053d074f1f51
tavodevcode <tavodevcode@gmail.com>
2023-11-18 14:10:13 -0600

    📝 Refactor app component HTML and import necessary components

--- src/app/app.component.ts
+++ src/app/app.component.ts
@@ -2,22 +2,22 @@ import { Component, OnInit } from '@angular/core';
 import { CommonModule } from '@angular/common';
 import { RouterOutlet } from '@angular/router';
 import { initFlowbite } from 'flowbite';
+import { NavigationComponent } from './navigation/navigation.component';
+import { UserFormComponent } from './components/user-form/user-form.component';
 
 @Component({
   selector: 'app-root',
   standalone: true,
-  imports: [CommonModule, RouterOutlet],
+  imports: [CommonModule, RouterOutlet, NavigationComponent, UserFormComponent],
   templateUrl: './app.component.html',
-  styleUrl: './app.component.scss'
+  styleUrl: './app.component.scss',
 })
 export class AppComponent implements OnInit {
   title = 'crm-app';
 
-
+  isLoading = true;
 
   ngOnInit(): void {
     initFlowbite();
-
-    console.log("initFlowbite");
   }
 }
