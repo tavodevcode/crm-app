708138485625e2eba397dfc1c8b376fd3819a96c
tavodevcode <tavodevcode@gmail.com>
2023-11-19 20:45:32 -0600

    📝 Update user interface and create user interface

--- src/app/interfaces/user.d.ts
+++ src/app/interfaces/user.d.ts
@@ -1,16 +1,16 @@
 export interface User {
-  id: string;
-  firstName: string;
-  lastName: string;
-  email: string;
-  phone: string;
-  address: string;
+  id: number
+  first_name: string
+  last_name: string
+  email: string
+  phone: string
+  address: string
 }
 
 export interface CreateUser {
-  firstName: string;
-  lastName: string;
-  email: string;
-  phone: string;
-  address: string;
+  first_name: string
+  last_name: string
+  email: string
+  phone: string
+  address: string
 }
